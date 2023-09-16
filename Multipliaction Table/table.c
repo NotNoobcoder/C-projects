92ec1699be9611f9090294482f86fc4cec649820
NotNoobcoder <144189450+NotNoobcoder@users.noreply.github.com>
2023-09-16 06:17:43 -0700

    create table.c

--- Multipliaction Table/table.c
+++ Multipliaction Table/table.c
@@ -7,5 +7,15 @@ int main()
     scanf("%d", &num_you_enter);
     total = num_you_enter;
     printf("%d x 1 = %d\n", total, num_you_enter * 1);
+    printf("%d x 2 = %d\n", total, num_you_enter * 2);
+    printf("%d x 3 = %d\n", total, num_you_enter * 3);
+    printf("%d x 4 = %d\n", total, num_you_enter * 4);
+    printf("%d x 5 = %d\n", total, num_you_enter * 5);
+    printf("%d x 6 = %d\n", total, num_you_enter * 6);
+    printf("%d x 7 = %d\n", total, num_you_enter * 7);
+    printf("%d x 8 = %d\n", total, num_you_enter * 8);
+    printf("%d x 9 = %d\n", total, num_you_enter * 9);
+    printf("%d x 10 = %d\n", total, num_you_enter * 10);
+
     return 0;
 }
